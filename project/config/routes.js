f92d8db73fefed1b0dd1f1fe4503467d3785457b
jamesjoel <james.joelj@gmail.com>
2018-10-10 12:44:40 +0530

    lookup in mongodb

--- project/config/routes.js
+++ project/config/routes.js
@@ -7,6 +7,7 @@ routes.use("/signup", require("../controllers/signup"));
 routes.use("/login", require("../controllers/login"));
 routes.use("/show", require("../controllers/show"));
 routes.use("/cart", require("../controllers/cart"));
+routes.use("/student", require("../controllers/student"));
 
 
 routes.use("/db", require("../controllers/db"));
--- project/models/student.js
+++ project/models/student.js
@@ -8,3 +8,21 @@ module.exports.find=function(obj, cb){
 		db.collection("student").find(obj).toArray(cb);
 	});
 }
+module.exports.findAggregate=function(array, cb){
+	con(function(err, client){
+		var db = client.db(config.database);
+		db.collection("student").aggregate(array).toArray(cb);
+	})
+}
+
+
+
+
+
+
+module.exports.lookup=function(array, cb){
+	con(function(err, client){
+		var db = client.db(config.database);
+		db.collection("cityTbl").aggregate(array).toArray(cb);
+	})
+}
