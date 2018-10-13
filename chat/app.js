5a3d464a273978013fe69fdce656a9f0a59a4f4d
jamesjoel <james.joelj@gmail.com>
2018-10-13 13:03:49 +0530

    chatting

--- chat/app.js
+++ chat/app.js
@@ -9,10 +9,28 @@ app.use(bodyParser());
 
 
 var arr = [];
-
+var name = "";
+var obj = {};
 
 io.on("connection", function(socket){
-	io.emit("onlineUser", arr);
+	
+	obj[socket.id]=name;
+	// arr.push(obj);
+
+
+
+	socket.emit("me", name);
+	io.emit("onlineUser", obj);
+
+	socket.on("chat", function(data){
+		// console.log(data);
+		var to = data.to;
+		var sendobj = { name : data.sender, msg : data.msg };
+
+		socket.broadcast.to(to).emit("chat", sendobj);
+	});
+
+
 	// sending all client
 });
 
@@ -21,7 +39,10 @@ app.get("/", function(req, res){
 });
 app.post("/", function(req, res){
 	// console.log(req.body);
-	arr.push(req.body.name);
+	name = req.body.name;
+	
+
+	
 	res.redirect("/chat");
 });
 
