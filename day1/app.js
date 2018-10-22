ae3d0122e33c3ae1bbe8bdaee3953ffa33826690
jamesjoel <james.joelj@gmail.com>
2018-10-22 13:58:42 +0530

    google maps

--- day1/app.js
+++ day1/app.js
@@ -2,12 +2,9 @@ var express = require("express");
 var app = express();
 var port = 3000;
 
-// console.log(__dirname);
-// console.log(__filename);
-// console.log(console);
-// console.log(process.version);
-// console.log(module);
-
+app.get("/", function(req, res){
+	res.sendFile(__dirname+"/index.html");
+});
 
 
 app.listen(port, function(){
