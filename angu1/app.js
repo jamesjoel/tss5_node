7cd50658ad6709477071534fe25b3a9db8a25c0c
jamesjoel <james.joelj@gmail.com>
2018-10-24 13:36:23 +0530

    angular ajax

--- angu1/app.js
+++ angu1/app.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var app = express();
+var MongoClient = require("mongodb").MongoClient;
+var bodyParser = require("body-parser");
 
 app.use(express.static(__dirname+"/public"));
+app.use(bodyParser());
 
 app.get("/", function(req, res){
 	res.sendFile(__dirname+"/index.html");
 });
 
+app.get("/getdata", function(req, res){
+	MongoClient.connect("mongodb://localhost:27017", function(err, client){
+		var db = client.db("tss5");
+		db.collection("student").find({}).toArray(function(err, result){
+			res.send(result);
+		});
+	});
+});
+
+app.post("/getdata", function(req, res){
+	MongoClient.connect("mongodb://localhost:27017", function(err, client){
+		var db = client.db("tss5");
+		db.collection("student").insert(req.body,function(err, result){
+			console.log(result);
+		});
+	});
+});
 
 
 app.listen(3000, function(){
--- angu1/public/js/app.js
+++ angu1/public/js/app.js
@@ -1,8 +1,24 @@
 var app = angular.module("myApp", []);
-app.controller("myCtrl", function($scope){
-	$scope.f_name = "rohit";
-	$scope.l_name = "sharma";
-	$scope.gender = "male";
-	
-	
+app.controller("myCtrl", function($scope, $http){
+	$scope.newData={};
+
+	$scope.data=[];
+	$http({
+		method : "get",
+		url : "/getdata"
+
+	}).then(function(res){
+		console.log(res.data);
+		$scope.data=res.data;
+	});
+
+	$scope.save=function(){
+		$http({
+			method : "post",
+			url : "/getdata",
+			data : $scope.newData
+		}).then(function(res){
+			console.log(res.data);
+		});
+	}
 });
